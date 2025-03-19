364e91633ac74053e61600490af5600515fb31b5
Njoroge Charles <njorodev1319@gmail.com>
2025-03-19 09:20:21 +0300

    initial home page interface

--- userend/src/pages/index.jsx
+++ userend/src/pages/index.jsx
@@ -5,6 +5,7 @@ import useSessionHook from "@/components/hooks/sessionHook";
 import KitchenHome from "@/components/KitchenHome";
 import ManagerHome from "@/components/ManagerHome";
 import WaiterHome from "@/components/WaiterHome";
+import Link from "next/link";
 import useUser from "../components/hooks/userHook";
 
 const Home = () => {
@@ -51,15 +52,29 @@ const Home = () => {
         </div>
       ) : isLoading ? (
         <div className="w-screen h-[calc(90vh)] flex flex-col justify-center items-center text-4xl font-body">
-          User Loading...
+          Loading session...
         </div>
       ) : error ? (
-        <div className="w-screen h-[calc(90vh)] flex flex-col justify-center items-center text-4xl font-body">
-          Error fetching user, login again...
+        <div className="w-screen h-[calc(90vh)] flex flex-col justify-center items-center  font-body bg-blue-50 gap-8">
+          <p className="text-4xl">
+            Previous session expired. Please login again
+          </p>
+          <Link
+            href="/api/auth/signin?callbackUrl=/"
+            className="font-body font-semi-bold bg-blue-100 px-10 py-2 rounded-full shadow-md"
+          >
+            Log In
+          </Link>
         </div>
       ) : (
-        <div className="w-screen h-[calc(90vh)] flex flex-col justify-center items-center text-4xl font-body">
-          You are not logged in
+        <div className="w-screen h-[calc(90vh)] flex flex-col justify-center items-center  font-body bg-blue-50 gap-8">
+          <p className="text-4xl">You are not logged in</p>
+          <Link
+            href="/api/auth/signin?callbackUrl=/"
+            className="font-body font-semi-bold bg-blue-100 px-10 py-2 rounded-full shadow-md"
+          >
+            Log In
+          </Link>
         </div>
       )}
     </div>
