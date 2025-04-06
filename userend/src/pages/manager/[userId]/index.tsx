ba2f9f52f2c2110a7df266b3584c2316abfbf157
Njoroge Charles <njorodev1319@gmail.com>
2025-04-06 16:00:33 +0300

    More on user performance check page

--- userend/src/pages/manager/[userId]/index.tsx
+++ userend/src/pages/manager/[userId]/index.tsx
@@ -1,11 +1,31 @@
 import axios from "axios";
+import Image from "next/image";
 
 const UserTracking = ({ currentUser }) => {
   return (
-    <div>
-      <h1>Track user</h1>
-      <p>User ID: {currentUser._id}</p>
-      <p>User Name: {currentUser.name}</p>
+    <div className="w-screen h-[calc(90vh)] flex flex-row justify-start items-start bg-blue-200">
+      <nav className="side-nav w-64 h-full flex flex-col justify-start items-start shadow-lg p-5 gap-4">
+        <Image
+          src={`${currentUser.profilePicture}`}
+          alt="Profile picture"
+          width={500}
+          height={500}
+          className="h-48 w-48 rounded-full object-cover shadow-lg mb-4" // Add border and rounded-full for styling
+        ></Image>
+        <h1 className="text-2xl font-bold font-body tracking-wider text-gray-800">
+          {currentUser.name}
+        </h1>
+        <p className="font-body font-semibold text-gray-600 capitalize tracking-widest">
+          Role: {currentUser.role}
+        </p>
+        <p className="font-heading font-semibold text-gray-500  tracking-widest">
+          {currentUser.email}
+        </p>
+
+        <p className="font-body font-semibold text-gray-600 capitalize tracking-widest">
+          Status: {currentUser.isActive ? "Online" : "Offline"}
+        </p>
+      </nav>
     </div>
   );
 };
